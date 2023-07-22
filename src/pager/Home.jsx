e9578415e3dab6ec9e80f6590d48c6f2a72a3848
ArtemPonkratov <artemka.g@mail.ru>
2023-07-22 15:28:49 +0200

    test1

--- src/pager/Home.jsx
+++ src/pager/Home.jsx
@@ -9,6 +9,7 @@ import num4 from '../img1/num4.png';
 
 function Home() {
   const [modalProductId, setModalProductId] = useState(null); // id выбранного товара для модального окна
+  const [modalImageIndex, setModalImageIndex] = useState(0); // индекс текущей картинки в модальном окне
 
 
 
@@ -22,6 +23,8 @@ function Home() {
     setModalProductId(null);
   };
 
+
+// Картинки для модального окна с id 1
   const modal1Images = [
     "./img/keks.jpg",
     "./img/1keks.jpg",
@@ -29,11 +32,35 @@ function Home() {
     "./img/3keks.jpg"
   ];
 
-  // Картинки для модального окна с id 2
+  
+ // Картинки для модального окна с id 2
+ const modal1Images2 = [
+  "./img/1keks.jpg",
+  "./img/2keks.jpg",
+  "./img/3keks.jpg"
+];
 
+ // Картинки для модального окна с id 3
+ const modal1Images3 = [
+  "./img/1keks.jpg",
+  "./img/2keks.jpg",
+  "./img/3keks.jpg"
+];
 
-  const [modalImageIndex, setModalImageIndex] = useState(0); // индекс текущей картинки в модальном окне
+ // Картинки для модального окна с id 4
+ const modal1Images4 = [
+  "./img/1keks.jpg",
+  "./img/2keks.jpg",
+  "./img/3keks.jpg"
+];
+   
 
+ // Обработчик клика вне модального окна для закрытия его
+ const handleModalClick = (event) => {
+  if (event.target.classList.contains("modal")) {
+    closeModal();
+  }
+};
  
   
 
@@ -58,21 +85,21 @@ function Home() {
           <img src="./img/keks.jpg" alt="Product" style={{ width: 400 }} />
           <div className="card-info">
             <h2>Шу</h2>
-            <button onClick={() => openModal(2)} className="button">Подробности</button>
+            <button onClick={() => openModal(3)} className="button">Подробности</button>
           </div>
         </div>
         <div className="card">
           <img src="./img/keks.jpg" alt="Product" style={{ width: 400 }} />
           <div className="card-info">
             <h2>Шу</h2>
-            <button onClick={() => openModal(2)} className="button">Подробности</button>
+            <button onClick={() => openModal(4)} className="button">Подробности</button>
           </div>
         </div>
       </div>
 
       {/* Модальное окно для товара с id 1 */}
       {modalProductId === 1 && (
-        <div className="modal">
+        <div className="modal" onClick={handleModalClick}>
         <div className="modal-content">
           <div className="modal-images">
             <img
@@ -123,7 +150,6 @@ function Home() {
               </div>
               <Link to="/" className="order-button">Заказать</Link>
             </div>
-            <button onClick={closeModal} className="close-button">Закрыть</button>
           </div>
         </div>
       )}
@@ -132,32 +158,169 @@ function Home() {
 
       {/* Модальное окно для товара с id 2 */}
       {modalProductId === 2 && (
-        <div className="modal">
-          <div className="modal-content">
-            <img src="./img/keks.jpg" alt="Product" className="product-image" style={{ width: 400 }} />
-            <div className="product-details">
-              <h2 className="product-name">Пицца</h2>
-              <p className="product-price">Цена: 500p</p>
-              <div className="product-toppings">
-                <p className='font'>1. Топпинги</p>
-                <select className="topping-select">
+        <div className="modal" onClick={handleModalClick}>
+        <div className="modal-content">
+          <div className="modal-images">
+            <img
+              src={modal1Images2[modalImageIndex] }
+              alt="Product"
+              className="modal-image"
+              
+            />
+            {modal1Images2.length > 1 && (
+              <div className="modal-navigator">
+                {modal1Images2.map((image, index) => (
+                  <img
+                    key={index}
+                    src={image}
+                    alt={`foto ${index}`}
+                    className={`modal-navigator-image ${index === modalImageIndex ? 'active' : ''}`}
+                    onClick={() => setModalImageIndex(index)}
+                  />
+                ))}
+              </div>
+            )}
+          </div>
+          <div className="product-details">
+              <h2 className="product-name">2</h2>
+              <p className="product-price">Цена: 350p</p>
+              <div className="product-filling">
+                <p className='font'>1. Капкейк</p>
+                <select className="filling-select">
                   <option className='select__head'>Выберите:</option>
-                  <option>Грибы</option>
-                  <option>Помидоры</option>
-                  <option>Сыр</option>
+                  <option>Ванильный</option>
+                  <option>Шоколадный</option>
+                </select>
+                <p className='font'>2. Начинка</p>
+                <select className="filling-select">
+                  <option className='select__head'>Выберите:</option>
+                  <option>Вишня</option>
+                  <option>Соленая карамель</option>
+                  <option>Без начинки</option>
                 </select>
               </div>
               <div className="product-quantity">
-                <p className='font'>2. Количество</p>
+                <p className='font'>3. Количество</p>
                 <select className="filling-select">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
+                  <option>6</option>
+                  <option>12</option>
+                  <option>24</option>
                 </select>
               </div>
               <Link to="/" className="order-button">Заказать</Link>
             </div>
-            <button onClick={closeModal} className="close-button">Закрыть</button>
+          </div>
+        </div>
+      )}
+
+{modalProductId === 3 && (
+        <div className="modal" onClick={handleModalClick}>
+        <div className="modal-content">
+          <div className="modal-images">
+            <img
+              src={modal1Images3[modalImageIndex] }
+              alt="Product"
+              className="modal-image"
+              
+            />
+            {modal1Images3.length > 1 && (
+              <div className="modal-navigator">
+                {modal1Images3.map((image, index) => (
+                  <img
+                    key={index}
+                    src={image}
+                    alt={`foto ${index}`}
+                    className={`modal-navigator-image ${index === modalImageIndex ? 'active' : ''}`}
+                    onClick={() => setModalImageIndex(index)}
+                  />
+                ))}
+              </div>
+            )}
+          </div>
+          <div className="product-details">
+              <h2 className="product-name">3</h2>
+              <p className="product-price">Цена: 350p</p>
+              <div className="product-filling">
+                <p className='font'>1. Капкейк</p>
+                <select className="filling-select">
+                  <option className='select__head'>Выберите:</option>
+                  <option>Ванильный</option>
+                  <option>Шоколадный</option>
+                </select>
+                <p className='font'>2. Начинка</p>
+                <select className="filling-select">
+                  <option className='select__head'>Выберите:</option>
+                  <option>Вишня</option>
+                  <option>Соленая карамель</option>
+                  <option>Без начинки</option>
+                </select>
+              </div>
+              <div className="product-quantity">
+                <p className='font'>3. Количество</p>
+                <select className="filling-select">
+                  <option>6</option>
+                  <option>12</option>
+                  <option>24</option>
+                </select>
+              </div>
+              <Link to="/" className="order-button">Заказать</Link>
+            </div>
+          </div>
+        </div>
+      )}
+
+{modalProductId === 4 && (
+        <div className="modal" onClick={handleModalClick}>
+        <div className="modal-content">
+          <div className="modal-images">
+            <img
+              src={modal1Images4[modalImageIndex] }
+              alt="Product"
+              className="modal-image"
+              
+            />
+            {modal1Images4.length > 1 && (
+              <div className="modal-navigator">
+                {modal1Images4.map((image, index) => (
+                  <img
+                    key={index}
+                    src={image}
+                    alt={`foto ${index}`}
+                    className={`modal-navigator-image ${index === modalImageIndex ? 'active' : ''}`}
+                    onClick={() => setModalImageIndex(index)}
+                  />
+                ))}
+              </div>
+            )}
+          </div>
+          <div className="product-details">
+              <h2 className="product-name">4</h2>
+              <p className="product-price">Цена: 350p</p>
+              <div className="product-filling">
+                <p className='font'>1. Капкейк</p>
+                <select className="filling-select">
+                  <option className='select__head'>Выберите:</option>
+                  <option>Ванильный</option>
+                  <option>Шоколадный</option>
+                </select>
+                <p className='font'>2. Начинка</p>
+                <select className="filling-select">
+                  <option className='select__head'>Выберите:</option>
+                  <option>Вишня</option>
+                  <option>Соленая карамель</option>
+                  <option>Без начинки</option>
+                </select>
+              </div>
+              <div className="product-quantity">
+                <p className='font'>3. Количество</p>
+                <select className="filling-select">
+                  <option>6</option>
+                  <option>12</option>
+                  <option>24</option>
+                </select>
+              </div>
+              <Link to="/" className="order-button">Заказать</Link>
+            </div>
           </div>
         </div>
       )}
